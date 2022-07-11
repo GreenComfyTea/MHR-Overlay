1e16e01fb6648a3ba642dd0bf5d1a61b8aa77d58
GreenComfyTea <klunics97@gmail.com>
2022-07-11 13:16:52 +0300

    Release v2.1

--- reframework/autorun/MHR_Overlay/Misc/config.lua
+++ reframework/autorun/MHR_Overlay/Misc/config.lua
@@ -4085,7 +4085,7 @@ function config.init_module()
 
 	config.init();
 	config.load();
-	config.current_config.version = "v2.0";
+	config.current_config.version = "v2.1";
 
 	language.update(table_helpers.find_index(language.language_names, config.current_config.global_settings.language, false));
 
