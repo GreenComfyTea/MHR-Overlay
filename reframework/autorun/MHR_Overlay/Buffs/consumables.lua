5c42502700eb58eaae436430c86f8507695b52cf
GreenComfyTea <klunics97@gmail.com>
2023-09-06 16:29:56 +0300

    Remove Might/Adamant Seeds (covered by Attack/Defense Up) and Dash Juice(covered by Stamina Use Down)

--- reframework/autorun/MHR_Overlay/Buffs/consumables.lua
+++ reframework/autorun/MHR_Overlay/Buffs/consumables.lua
@@ -47,12 +47,12 @@ this.list = {
 	mega_demondrug = nil,
 	armorskin = nil,
 	mega_armorskin = nil,
-	might_seed = nil,
-	adamant_seed = nil,
+	-- might_seed = nil,
+	-- adamant_seed = nil,
 	demon_powder = nil,
 	hardshell_powder = nil,
 	immunizer = nil,
-	dash_juice = nil,
+	-- dash_juice = nil,
 	gourmet_fish = nil,
 };
 
@@ -61,12 +61,12 @@ local consumable_ids = {
 	mega_demondrug = 68157918,
 	armorskin = 68157922,
 	mega_armorskin = 68157923,
-	might_seed = 68157919,
-	adamant_seed = 68157924,
+	--might_seed = 68157919,
+	--adamant_seed = 68157924,
 	demon_powder = 68157920,
 	hardshell_powder = 68157925,
 	immunizer = 68157911,
-	dash_juice = 68157913,
+	--dash_juice = 68157913,
 	gourmet_fish = 68157909
 }
 
@@ -98,12 +98,6 @@ local player_data_type_def = sdk.find_type_definition("snow.player.PlayerData");
 local atk_up_alive_field = player_data_type_def:get_field("_AtkUpAlive");
 -- Armorskin/Mega Armorskin
 local def_up_alive_field = player_data_type_def:get_field("_DefUpAlive");
--- Might Seed
-local atk_up_buff_second_field = player_data_type_def:get_field("_AtkUpBuffSecond");
-local atk_up_buff_second_timer_field = player_data_type_def:get_field("_AtkUpBuffSecondTimer");
--- Adamant Seed
-local def_up_buff_second_field = player_data_type_def:get_field("_DefUpBuffSecond");
-local def_up_buff_second_timer_field = player_data_type_def:get_field("_DefUpBuffSecondTimer");
 -- Demon Powder
 local atk_up_item_second_field = player_data_type_def:get_field("_AtkUpItemSecond");
 local atk_up_item_second_timer_field = player_data_type_def:get_field("_AtkUpItemSecondTimer");
@@ -112,8 +106,6 @@ local def_up_item_second_field = player_data_type_def:get_field("_DefUpItemSecon
 local def_up_item_second_timer_field = player_data_type_def:get_field("_DefUpItemSecondTimer");
 -- Immunizer
 local vitalizer_timer_field = player_data_type_def:get_field("_VitalizerTimer");
--- Dash Juice
-local stamina_up_buff_second_timer_field = player_data_type_def:get_field("_StaminaUpBuffSecondTimer");
 -- Gourmet Fish
 local fish_regene_enable_field = player_data_type_def:get_field("_FishRegeneEnableTimer");
 
@@ -131,11 +123,6 @@ function this.update(player_data)
 
 	this.update_demondrug(player_data, item_parameter);
 	this.update_armorskin(player_data, item_parameter);
-	this.update_might_seed(player_data, item_parameter);
-	this.update_dash_juice(player_data, item_parameter);
-	
-	buffs.update_generic_buff(this.list, consumables_type_name, "adamant_seed", this.get_consumable_name,
-		player_data, def_up_buff_second_field, player_data, def_up_buff_second_timer_field, item_parameter, adamant_seed_timer_field);
 	
 	buffs.update_generic_buff(this.list, consumables_type_name, "demon_powder", this.get_consumable_name,
 		player_data, atk_up_item_second_field, player_data, atk_up_item_second_timer_field, item_parameter, demondrug_powder_timer_field);
@@ -224,60 +211,6 @@ function this.update_armorskin(player_data, item_parameter)
 	end
 
 	buffs.update_generic(this.list, consumables_type_name, consumable_key, this.get_consumable_name);
-end
-
-function this.update_might_seed(player_data, item_parameter)
-	local atk_up_buff_second = atk_up_buff_second_field:get_data(player_data);
-	if atk_up_buff_second == nil then
-		error_handler.report("consumables.update_might_seed", "Failed to access Data: atk_up_buff_second");
-		return;
-	end
-
-	local might_seed_atk_up = might_seed_atk_up_field:get_data(item_parameter);
-	if might_seed_atk_up == nil then
-		error_handler.report("consumables.update_might_seed", "Failed to access Data: might_seed_atk_up");
-		return;
-	end
-
-	if atk_up_buff_second ~= might_seed_atk_up then
-		this.list.might_seed = nil;
-		return;
-	end
-
-	this.update_generic_buff(this.list, consumables_type_name, "might_seed", this.get_consumable_name,
-		nil, nil, player_data, atk_up_buff_second_timer_field, item_parameter, might_seed_timer_field);
-end
-
-function this.update_dash_juice(player_data, item_parameter)
-	local stamina_up_buff_second_timer = stamina_up_buff_second_timer_field:get_data(player_data);
-	if stamina_up_buff_second_timer == nil then
-		error_handler.report("consumables.update_dash_juice", "Failed to access Data: stamina_up_buff_second_timer");
-		return;
-	end
-
-	local stamina_up_buff_second = stamina_up_buff_second_field:get_data(item_parameter);
-	if stamina_up_buff_second == nil then
-		error_handler.report("consumables.update_dash_juice", "Failed to access Data: stamina_up_buff_second");
-		return;
-	end
-
-	if utils.number.is_equal(stamina_up_buff_second_timer, 0) then
-		this.list.dash_juice = nil;
-		return;
-	end
-
-	local timer = stamina_up_buff_second_timer / 60;
-	local dash_juice_consumable = this.list.dash_juice;
-
-	if dash_juice_consumable == nil
-	or (dash_juice_consumable ~= nil and timer > dash_juice_consumable.timer) then
-		if timer <= endemic_life_buffs.peepersects_duration + 0.1 then
-			this.list.dash_juice = nil;
-			return;
-		end
-	end
-
-	buffs.update_generic(this.list, consumables_type_name, "dash_juice", this.get_consumable_name, 1, timer, stamina_up_buff_second);
 end
 
 function this.get_consumable_name(consumable_key)
