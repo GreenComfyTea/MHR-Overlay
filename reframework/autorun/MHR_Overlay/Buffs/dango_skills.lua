590b3b17c2a051ea409363e3dca0f16d5b6dbb14
GreenComfyTea <klunics97@gmail.com>
2023-09-06 16:27:17 +0300

    Remove Dango Bulker (covered by Attack Up)

--- reframework/autorun/MHR_Overlay/Buffs/dango_skills.lua
+++ reframework/autorun/MHR_Overlay/Buffs/dango_skills.lua
@@ -44,7 +44,6 @@ local package = package;
 this.list = {
 	dango_adrenaline = nil,
 	dango_booster = nil,
-	dango_bulker = nil,
 	dango_insurance = nil,
 	dango_insurance_defense_up = nil,
 	dango_glutton = nil,
@@ -56,60 +55,60 @@ this.list = {
 };
 
 local dango_ids = {
-	dango_polisher = 1,
-	dango_rider = 2,
+	-- dango_polisher = 1,
+	-- dango_rider = 2,
 	dango_adrenaline = 3,
-	dango_carver_lo = 4,
-	dango_carver_hi = 5,
-	dango_medic_lo = 6,
-	dango_medic_hi = 7,
-	dango_fighter = 8,
-	dango_pyro = 9,
-	dango_specialist = 10,
-	dango_defender_lo = 11,
-	dango_defender_hi = 12,
-	dango_harvester = 13,
-	dango_marksman = 14,
-	dango_fortune_caller = 15,
-	dango_miracle_worker = 16,
-	dango_deflector = 17,
-	dango_weakener = 18,
-	dango_calculator = 19,
-	dango_temper = 20,
-	dango_wall_runner = 21,
-	dango_slugger = 22,
-	dango_money_maker = 23,
-	dango_bombardier = 24,
-	dango_moxie = 25,
-	dango_immunizer = 26,
-	dango_trainer = 27,
+	-- dango_carver_lo = 4,
+	-- dango_carver_hi = 5,
+	-- dango_medic_lo = 6,
+	-- dango_medic_hi = 7,
+	-- dango_fighter = 8,
+	-- dango_pyro = 9,
+	-- dango_specialist = 10,
+	-- dango_defender_lo = 11,
+	-- dango_defender_hi = 12,
+	-- dango_harvester = 13,
+	-- dango_marksman = 14,
+	-- dango_fortune_caller = 15,
+	-- dango_miracle_worker = 16,
+	-- dango_deflector = 17,
+	-- dango_weakener = 18,
+	-- dango_calculator = 19,
+	-- dango_temper = 20,
+	-- dango_wall_runner = 21,
+	-- dango_slugger = 22,
+	-- dango_money_maker = 23,
+	-- dango_bombardier = 24,
+	-- dango_moxie = 25,
+	-- dango_immunizer = 26,
+	-- dango_trainer = 27,
 	dango_booster = 28,
-	dango_feet = 29,
-	dango_bulker = 30,
+	-- dango_feet = 29,
+	-- dango_bulker = 30,
 	dango_insurance = 31,
-	dango_reviver = 32,
-	dango_summoner = 33,
-	dango_hurler = 34,
-	dango_fire_res_lo = 35,
-	dango_fire_res_hi = 36,
-	dango_water_res_lo = 37,
-	dango_water_res_hi = 38,
-	dango_thunder_res_lo = 39,
-	dango_thunder_res_hi = 40,
-	dango_ice_res_lo = 41,
-	dango_ice_res_hi = 42,
-	dango_dragon_res_lo = 43,
-	dango_dragon_res_hi = 44,
-	dango_gatherer = 45,
+	-- dango_reviver = 32,
+	-- dango_summoner = 33,
+	-- dango_hurler = 34,
+	-- dango_fire_res_lo = 35,
+	-- dango_fire_res_hi = 36,
+	-- dango_water_res_lo = 37,
+	-- dango_water_res_hi = 38,
+	-- dango_thunder_res_lo = 39,
+	-- dango_thunder_res_hi = 40,
+	-- dango_ice_res_lo = 41,
+	-- dango_ice_res_hi = 42,
+	-- dango_dragon_res_lo = 43,
+	-- dango_dragon_res_hi = 44,
+	-- dango_gatherer = 45,
 	dango_glutton = 46,
-	dango_bird_caller = 47,
+	-- dango_bird_caller = 47,
 	dango_flyer = 48,
 	dango_defender = 49,
-	enhanced_dango_fighter = 50,
-	dango_driver = 51,
+	-- enhanced_dango_fighter = 50,
+	-- dango_driver = 51,
 	dango_hunter = 52,
-	dango_guard = 53,
-	dango_shifter = 54,
+	-- dango_guard = 53,
+	-- dango_shifter = 54,
 	dango_connector = 55,
 	super_recovery_dango = 56
 };
@@ -136,9 +135,6 @@ local is_enable_kitchen_skill_048_reduce_method = player_data_type_def:get_field
 local kitchen_skill_027_timer_field = player_data_type_def:get_field("_KitchenSkill027Timer");
 -- Dango Glutton
 local kitchen_skill_045_timer_field = player_data_type_def:get_field("_KitchenSkill045Timer");
--- Dango Bulker
-local atk_up_buff_second_field = player_data_type_def:get_field("_AtkUpBuffSecond");
-local atk_up_buff_second_timer_field = player_data_type_def:get_field("_AtkUpBuffSecondTimer");
 -- Dango Insurance
 local kitchen_skill_insurance_def_up_lv3_field = player_data_type_def:get_field("_KitchenSkill_Insurance_DefUp_Lv3");
 local kitchen_skill_insurance_def_up_lv4_field = player_data_type_def:get_field("_KitchenSkill_Insurance_DefUp_Lv4");
@@ -182,7 +178,6 @@ function this.update(player, player_data)
 	end
 
 	this.update_dango_adrenaline();
-	this.update_dango_bulker(player_data);
 	this.update_dango_hunter(player_data);
 	this.update_dango_insurance();
 	this.update_dango_insurance_defense_up(player_data);
@@ -209,21 +204,6 @@ function this.update_dango_adrenaline()
 	end
 
 	buffs.update_generic(this.list, dango_skills_type_name, "dango_adrenaline", this.get_dango_name);
-end
-
-function this.update_dango_bulker(player_data)
-	local atk_up_buff_second = atk_up_buff_second_field:get_data(player_data);
-	if atk_up_buff_second == nil then
-		error_handler.report("consumables.update_dango_bulker", "Failed to access Data: atk_up_buff_second");
-		return;
-	end
-
-	if atk_up_buff_second ~= dango_bulker_attack_up then
-		this.list.dango_bulker = nil;
-		return;
-	end
-
-	buffs.update_generic_buff(this.list, dango_skills_type_name, "dango_bulker", this.get_dango_name, nil, nil, player_data, atk_up_buff_second_timer_field);
 end
 
 function this.update_dango_insurance()
