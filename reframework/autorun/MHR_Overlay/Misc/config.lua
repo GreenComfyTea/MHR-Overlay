3b89ce8303b02ad36511d49c85983466aa54188c
GreenComfyTea <klunics97@gmail.com>
2023-10-24 10:15:42 +0300

    Bump version to v2.6.2

--- reframework/autorun/MHR_Overlay/Misc/config.lua
+++ reframework/autorun/MHR_Overlay/Misc/config.lua
@@ -36,7 +36,7 @@ local os = os;
 local ValueType = ValueType;
 local package = package;
 
-this.version = "2.6.1";
+this.version = "2.6.2";
 
 this.config_folder = "MHR Overlay\\configs\\";
 this.current_config_value_file_name = "MHR Overlay\\config.json";
